0a57739e3c786ba9598c52397d6704094c2658e9
Amy Zhu <ajyzhu@gmail.com>
2022-02-11 22:07:31 -0800

    Convert everything so the webgl widget shows up

--- pattern-editor/src/index.js
+++ pattern-editor/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Viewer from "./Viewer";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
+);
+
+ReactDOM.render(
+    <Viewer />,
+  document.getElementById("viewer")
 );
